6aecfbabda5365a06d53e5540aee57da3fc4deb6
sven <sven.song@qq.com>
2015-09-29 13:17:04 +0800

    搭建项目框架

--- 9.29新闻/9.29新闻/Class/News/NewsTableViewController.h
+++ 9.29新闻/9.29新闻/Class/News/NewsTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  NewsTableViewController.h
 //  9.29新闻
 //
 //  Created by sven on 15/9/29.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface NewsTableViewController : UITableViewController
 
 @end
-
